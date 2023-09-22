875c85d219779c77477d378577d060f21f24a0e8
Joe Mafrici <hello@joemafrici.com>
2023-09-21 21:52:47 -0700

    working on two sum. add gitignore

--- 1TwoSum/main.cpp
+++ 1TwoSum/main.cpp
@@ -6,11 +6,18 @@ vector<int> twoSum(vector<int>& nums, int target);
 
 int main (int argc, char *argv[]) {
 	std::vector<int> nums {2, 7, 11, 15};
+	std::vector<int> nums2 {3, 2, 4};
+	std::vector<int> nums3 {3, 3};
 	int target = 9;
+	int target2 = 6;
+	int target3 = 6;
 	vector<int> result = twoSum(nums, target);
-	std::cout << result[0] << std::endl;
-	std::cout << result[1] << std::endl;
-	printf("%d\t%d", result[0], result[1]);
+	std::cout << "first " << result[0] << " " << result[1] << std::endl;
+	vector<int> result2 = twoSum(nums2, target2);
+	std::cout << "second " << result2[0] << " " << result2[1] << std::endl;
+	vector<int> result3 = twoSum(nums3, target3);
+	std::cout << "third " << result3[0] << " " << result3[1] << std::endl;
+
 	return 0;
 }
 
@@ -18,7 +25,12 @@ vector<int> twoSum(vector<int>& nums, int target)
 {
 	int firstIndex;
 	int secondIndex;
-	for (auto ii = 0; ii < nums.size(); ++ii)
+	
+	for (auto ii = 0; ii < nums.size() - 1; ++ii)
+	{
+		
+	}
+	for (auto ii = 0; ii < nums.size() - 1; ++ii)
 	{
 		if (nums[ii] + nums[ii + 1] == target)
 		{
