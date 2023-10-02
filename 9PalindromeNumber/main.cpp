866652ce357502d003ec29e3e5feac0dbb3c812f
Joe Mafrici <hello@joemafrici.com>
2023-10-02 08:32:44 -0700

    working on palindrome

--- 9PalindromeNumber/main.cpp
+++ 9PalindromeNumber/main.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
+#include <cmath>
+#include <vector>
 using namespace std;
 
 bool isPalindrome(int x);
@@ -18,15 +20,23 @@ int main()
 bool isPalindrome(int x)
 {
 	if (x < 0) return false;
+	int magnitude = log10(x) + 1;
+	vector<int> digits(magnitude);
+	for (auto left = 0, right = magnitude - 1; left < magnitude; ++left)
+	{
+		digits[left] = x % 10;
+		digits[right] = x %  int(pow(10, magnitude - 2));
+		cout << "power thing " << int(pow(10, magnitude - 2)) << endl;
+		cout << digits[left] << endl;
+		cout << digits[right] << endl;
+		//x /= 10;
+	}
 
-	cout << x % 10 << endl;
-	x /= 10;
-	cout << x % 10 << endl;
-	x /= 10;
-	cout << x % 10 << endl;
-	x /= 10;
-	cout << x % 10 << endl;
-	x /= 10;
-	cout << x % 10 << endl;
+	/*
+	for (auto ii = 0; ii < digits.size(); ++ii)
+	{
+		cout << digits[ii] << endl;
+	}
+	*/	
 	return true;
 }
