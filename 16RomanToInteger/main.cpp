b5891d1f2a72f3dc9cf779f32fb5663e6395fac3
Joe Mafrici <hello@joemafrici.com>
2023-10-03 14:21:27 -0700

    start roman to integer

---
+++ 16RomanToInteger/main.cpp
@@ -0,0 +1,22 @@
+#include <string>
+#include <cstring>
+using namespace std;
+class Solution {
+public:
+	int romanToInt(string s) {
+		for (auto ii = 0; ii < s.size(); ++ii)
+		{
+			if (strcmp(s.at(ii), "I"))
+			{
+
+			}
+			switch (s.at(ii)) {
+				case "I":
+			}
+		}
+	}
+};
+int main()
+{
+	Solution();
+}
