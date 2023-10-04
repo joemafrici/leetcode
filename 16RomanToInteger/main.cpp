060e7b05ea00f4128459d7814ad0221a3ce2aba3
Joe Mafrici <hello@joemafrici.com>
2023-10-03 17:55:07 -0700

    solved

--- 16RomanToInteger/main.cpp
+++ 16RomanToInteger/main.cpp
@@ -1,22 +1,78 @@
 #include <string>
-#include <cstring>
+#include <iostream>
 using namespace std;
 class Solution {
 public:
 	int romanToInt(string s) {
-		for (auto ii = 0; ii < s.size(); ++ii)
+		int num = 0;
+		int ii = 0;
+		while (ii < s.size())
 		{
-			if (strcmp(s.at(ii), "I"))
-			{
-
-			}
 			switch (s.at(ii)) {
-				case "I":
+				case 'I':
+					if (ii + 1 < s.size() && s.at(ii + 1) == 'V')
+					{
+						num += 4;
+						++ii;
+					}
+					else if (ii + 1 < s.size() && s.at(ii + 1) == 'X')
+					{
+						num += 9;
+						++ii;
+					}
+					else num += 1;
+					break;
+				case 'V':
+					num += 5;
+					break;
+				case 'X':
+					if (ii + 1 < s.size() && s.at(ii + 1) == 'L')
+					{
+						num += 40;
+						++ii;
+					}
+					else if (ii + 1 < s.size() && s.at(ii + 1) == 'C')
+					{
+						num += 90;
+						++ii;
+					}
+					else {
+						num += 10;
+					}
+					break;
+				case 'L':
+					num += 50;
+					break;
+				case 'C':
+					if (ii + 1 < s.size() && s.at(ii + 1) == 'D')
+					{
+						num += 400;
+						++ii;
+					}
+					else if (ii + 1 < s.size() && s.at(ii + 1) == 'M')
+					{
+						num += 900;
+						++ii;
+					}
+					else {
+						num += 100;
+					}
+					break;
+				case 'D':
+					num += 500;
+					break;
+				case 'M':
+					num += 1000;
+					break;
 			}
+			++ii;
 		}
+		return num;
 	}
 };
 int main()
 {
-	Solution();
+	cout << Solution().romanToInt("III") << endl;
+	cout << Solution().romanToInt("LVIII") << endl;
+	cout << Solution().romanToInt("MCMXCIV") << endl;
 }
